87f344cccea47adfdf6dd32b8314af7ad5c8a4d2
LeinnarF <hilariofranniel@gmail.com>
2024-04-16 21:53:05 +0800

    Search bar position

--- src/main/java/org/nice/Panels/Header.java
+++ src/main/java/org/nice/Panels/Header.java
@@ -10,7 +10,7 @@ import java.awt.*;
 
 public class Header extends JPanel {
     public Header() {
-        setLayout(new MigLayout("", "grow"));
+        setLayout(new MigLayout("", "1.5%[grow][30%]"));
         setBorder(
                 new FlatLineBorder(
                         new Insets(0,0,1,0),
@@ -21,14 +21,14 @@ public class Header extends JPanel {
     }
 
     private void init() {
-        var leftside = new JPanel(new MigLayout("al left", "[][grow]"));
+        var leftside = new JPanel(new MigLayout("align left", "[grow]"));
 
         add(leftside, "grow");
-        leftside.add(new JLabel(""), "align left");
-        leftside.setBackground(new Color(0xE46666));
+        //leftside.add(new JLabel(""), "align left");
+        leftside.setBackground(new Color(0xE46666)); //0xE46666
         // Search bar
-        leftside.add(new SearchBar(), "grow, align right");
-        leftside.add(new FilterBtn(), "align right");
+        leftside.add(new SearchBar(), "grow, align left");
+        leftside.add(new FilterBtn(), "align left");
 
     }
 }
