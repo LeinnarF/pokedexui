b418058a89d98188082280ead0b620e89d69bfff
Richard <richard232manansala@gmail.com>
2024-04-16 11:09:41 +0800

    Pasted the Footer changes from joben

--- src/main/java/org/nice/Panels/Footer.java
+++ src/main/java/org/nice/Panels/Footer.java
@@ -1,20 +1,25 @@
 package org.nice.Panels;
 
-import java.awt.*;
-import javax.swing.*;
-
 import net.miginfocom.swing.MigLayout;
 
-public class Footer extends JPanel{
-    public Footer() {
-        setLayout(new MigLayout("align left center"));
-        
-        JLabel label = new JLabel("Pokedex");
-        label.setFont(new Font("Inter", Font.BOLD, 20));
-        label.setForeground(Color.WHITE);
-        add(label, "gapleft 10");
+import java.awt.Color;
+import java.awt.Font;
 
+import javax.swing.*;
+import org.nice.Utils;
+
+public class Footer extends JPanel {
+    public Footer() {
+        setLayout(new MigLayout(""));
         setBackground(new Color(0xE46666));
-        setPreferredSize(new Dimension(0, 57));
+
+        ImageIcon pokeball = new ImageIcon(Utils.getResource("/images/items/4.png"));
+        add(new JLabel(pokeball), "align left center");
+
+        JLabel pokedex = new JLabel();
+        pokedex.setText("POKEDEX");
+        pokedex.setForeground(Color.WHITE);
+        pokedex.setFont(new Font("Arial", Font.BOLD, 16));
+        add(pokedex);
     }
 }
