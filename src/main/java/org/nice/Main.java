9e8ddf8c37391608248e5c23a3635f027f80887e
Richard <richard232manansala@gmail.com>
2024-04-18 15:50:40 +0800

    Added loading bar in loading frame

--- src/main/java/org/nice/Main.java
+++ src/main/java/org/nice/Main.java
@@ -4,27 +4,20 @@ import com.formdev.flatlaf.FlatIntelliJLaf;
 //import net.miginfocom.swing.MigLayout;
 
 import java.awt.*;
+import java.util.concurrent.TimeUnit;
 import javax.swing.*;
 
 import net.miginfocom.swing.MigLayout;
 import org.nice.Panels.*;
 import org.nice.services.PokemonService;
+import rx.Observable;
 
 public class Main {
     public static void main(String[] args) {
         FlatIntelliJLaf.setup();
 
-        JFrame frame = new JFrame("Pokedex - Loading");
-        ImageIcon AppIcon = new ImageIcon(Utils.getResource("/images/items/4.png"));
-        frame.setIconImage(AppIcon.getImage());
-
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(480, 120);
-        frame.setMinimumSize(new Dimension(480, 120)); // 720 + 30
-        frame.setLocationRelativeTo(null); // center the window
-        frame.setLayout(new MigLayout("align center center"));
-        frame.add(new JLabel("Initializing... Please wait"));
-        frame.setVisible(true);
+        var interval = Observable.timer(10, TimeUnit.MILLISECONDS);
+        JFrame loadingFrame = initLoadingFrame(interval);
         PokemonService.init();
 
         var footer = new Footer();
@@ -32,8 +25,29 @@ public class Main {
         var body = new Body();
 
         initMainFrame(header, footer, body);
-        frame.dispose();
+        loadingFrame.dispose();
 
+    }
+
+    private static JFrame initLoadingFrame(Observable<Long> interval) {
+        JFrame frame = new JFrame("Pokedex - Loading");
+        ImageIcon AppIcon = new ImageIcon(Utils.getResource("/images/items/4.png"));
+        frame.setIconImage(AppIcon.getImage());
+
+        frame.setUndecorated(true);
+        frame.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
+        frame.setSize(480, 120);
+        frame.setMinimumSize(new Dimension(480, 120)); // 720 + 30
+        frame.setLocationRelativeTo(null); // center the window
+        frame.setLayout(new MigLayout("align center center"));
+        frame.add(new JLabel("Initializing... Please wait"));
+
+        var loadingBar = new JProgressBar();
+        loadingBar.setIndeterminate(true);
+        loadingBar.setForeground(Color.red);
+        frame.add(loadingBar);
+        frame.setVisible(true);
+        return frame;
     }
 
     private static void initMainFrame(Header header, Footer footer, Body body) {
